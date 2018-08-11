6d33a71f022cb658434d65e4187471f53e36160a
raymond-lam <ray@lam-ray.com>
2018-08-11 15:12:24 -0400

    clarify comments

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 // to the resolved values of the promises in the given iterable, and if any of
 // the elements in the iterable are not Promises (or do not have a .then()
 // method) then those elements are treated as though they are wrapped in
-// Promise.resolve(). Unliked with Promise.all(), however, the returned Promise
+// Promise.resolve(). Unlike with Promise.all(), however, the returned Promise
 // rejects with the reason of the first promise that rejects after all of the
 // promises have either resolved or rejected, rather than immediately.
 module.exports = async promises => {
@@ -22,7 +22,7 @@ module.exports = async promises => {
   let exception;
   const values = [];
 
-  // As in Promise.all, throws an exception if promises is not iterable.
+  // As in Promise.all(), throws an exception if promises is not iterable.
   for (const promise of promises) {
     try {
       // If promise rejects or has already rejected, the rejection reason
@@ -30,7 +30,11 @@ module.exports = async promises => {
       // to contain enough elements, which is okay since an values is never
       // returned if any of the promises rejects anyhow. If promise resolves
       // or has already resolved, the resolve value is pushed onto the values
-      // array in the correct ordering.
+      // array in the correct ordering. The promises resolve or reject on their
+      // own asynchronously -- the await-in-a-loop does not in any way serialize
+      // or synchronize their execution. The await-in-a-loop's simply puts the
+      // resolution values of the promises into the values array in the correct
+      // order.
       values.push(await promise);
     }
     catch (e) {
@@ -46,8 +50,8 @@ module.exports = async promises => {
     }
   }
 
-  // If there was an exception, re-throw it only  after all the promises have
-  // resolved or rejected. async functions always return a promise, and throwing
+  // If there was an exception, re-throw it only after all the promises have
+  // resolved or rejected. async functions always return a Promise, and throwing
   // an exception is equivalent to rejecting, while returning is equivalent to
   // resolving.
   if (exceptionWasCaught) throw exception;
